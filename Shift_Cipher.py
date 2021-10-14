2f6c0ee7ac99e02ea693f1497ba21c83c1f7c50d
LeeJony <74480444+LeeJony000@users.noreply.github.com>
2021-10-14 19:28:58 +0800

    Update Shift_Cipher.py
    Add brute function

--- Shift_Cipher.py
+++ Shift_Cipher.py
@@ -1,30 +1,35 @@
 import sys
 import platform
 
+
 # 加密
 def shift_cipher_encrypt():
     list_s = []
     r_move = int(input('请输入加密移位参数(右移)：  '))
     s = input('请输入需要加密的字符： ')
     print("密文:", end='')
+    c = ""
     for i in s:
         list_s.append(ord(i))
     for i in list_s:
         i += r_move
         while i > 127:
             i -= 128
-        print(chr(i), end='')
+        c += chr(i)
+    return c
 
 
 def shift_cipher_encrypt(s, r_move):
     list_s = []
+    c = ""
     for i in s:
         list_s.append(ord(i))
     for i in list_s:
         i += r_move
         while i > 127:
             i -= 128
-        print(chr(i), end='')
+        c += chr(i)
+    return c
 
 
 # 解密
@@ -34,24 +39,38 @@ def shift_cipher_decrypt():
     l_move = int(input('请输入解密移位参数(左移)： '))
     s = input('请输入需要解密的字符： ')
     list_s = []
+    m = ""
     for i in s:
         list_s.append(ord(i))
     for i in list_s:
         i -= l_move
         while i < 0:
             i += 128
-        print(chr(i), end='')
+        m += chr(i)
+    return m
 
 
 def shift_cipher_decrypt(s, l_move):
     list_s = []
+    m = ""
     for i in s:
         list_s.append(ord(i))
     for i in list_s:
         i -= l_move
         while i < 0:
             i += 128
-        print(chr(i), end='')
+        m += chr(i)
+    return m
+
+
+# 爆破密文
+def shift_brute(s, match_str):
+    result = []
+    for k in range(128):
+        tmp = shift_cipher_decrypt(s, k)
+        if match_str in tmp:
+            result.append(tmp)
+    return result
 
 
 if __name__ == '__main__':
@@ -66,6 +85,7 @@ if __name__ == '__main__':
 
     if platform.system() == 'Windows':
         import publish_logo
+
         publish_logo.printSkyBlue(logo)
     elif platform.system() == 'Linux':
         print(f"\033[36m{logo}\033[0m")
@@ -77,23 +97,16 @@ if __name__ == '__main__':
         if answer.upper() == 'E':
             s = input('请输入需要加密的字符： ')
             r_move = int(input('请输入加密移位参数(右移)：'))
-            shift_cipher_encrypt(s, r_move)
-            print('\n')
+            print(shift_cipher_encrypt(s, r_move) + '\n')
         elif answer.upper() == 'D':
             s = input('请输入需要解密的字符： ')
             l_move = input('请输入解密移位参数(左移)，如果不知道请输入N：')
-            if l_move == 'N':
-                print("可能的明文:")
-                while i <= 25:
-                    shift_cipher_decrypt(s, i)
-                    print('\r')
-                    i = i + 1
-                print('\n')
+            if l_move.upper() == 'N':
+                match_str = input('请输入您需要匹配的字符串，没有请不填：')
+                print("可能的明文:" + shift_brute(s, match_str))
             elif l_move.isnumeric():
                 l_move = int(l_move)
-                print("明文为:", end="")
-                shift_cipher_decrypt(s, l_move)
-                print('\n')
+                print("明文为:" + shift_cipher_decrypt(s, l_move), end="\n")
         elif answer.upper() == 'Q':
             sys.exit()
         else:
